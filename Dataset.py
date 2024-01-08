a7e5dc7cade687112ff760e4296e174bf1c985b6
wooseungw <woow070840@gmail.com>
2024-01-09 02:45:32 +0900

    122

---
+++ Dataset.py
@@ -0,0 +1,33 @@
+import os
+import torch
+import json
+import numpy as np
+from torch.utils.data import Dataset
+"""주소 불러오는 함수"""
+def get_subdirectories(parent_directory):
+    """주어진 디렉토리 내의 모든 하위 디렉토리의 경로를 리스트로 반환합니다."""
+    subdirectories = [os.path.join(parent_directory, d) for d in os.listdir(parent_directory) 
+                    if os.path.isdir(os.path.join(parent_directory, d))]
+    return subdirectories
+
+def get_all_subdirectories(parent_directory):
+    """주어진 디렉토리 내의 첫 번째 및 두 번째 레벨 하위 디렉토리의 경로를 NumPy ndarray로 반환합니다."""
+    first_level_subdirs = get_subdirectories(parent_directory)
+    all_subdirs = []
+
+    for subdir in first_level_subdirs:
+        second_level_subdirs = get_subdirectories(subdir)
+        all_subdirs.extend(second_level_subdirs)  # 두 번째 레벨의 디렉토리를 추가
+
+    return np.array(all_subdirs)
+        
+class BaseDataset(Dataset):
+    def __init__(self, loc = os.path.join(os.getcwd(),"Dataset/Train/Image"), istrain = True):
+        self.loc = loc if istrain else loc.replace("Train","Valid")
+        self.dir_names = get_all_subdirectories(self.loc)
+        
+    def __len__(self): return len(self.dir_names)
+    
+class QADataset(BaseDataset):
+    def __getitem__(self, index):
+        return 
